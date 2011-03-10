1378f7aa45f7d8ff5682542c0fc44201ea61e989
lishen <lishen2@163.com>
2011-03-11 07:40:35 +0800

    add another test case

--- main.cpp
+++ main.cpp
@@ -8,14 +8,10 @@ using std::string;
 using ExcelFormula::Token;
 using ExcelFormula::FormulaParser;
 
-int main(int argc, char* argv[])
+void printParser(FormulaParser& parser)
 {
-	const char szFormula[] = "=1+3+5";
-	FormulaParser parser1(szFormula);
-	parser1.parserToToken1();
-	vector<Token*> tokens = parser1.getTokens();
-
-	cout << "Formual:" << szFormula << endl;
+	vector<Token*> tokens = parser.getTokens();
+	cout << "Formual:" << parser.getFormula() << endl;
 	cout << "Tokens:" << endl;
 	
 	for(vector<Token*>::iterator it = tokens.begin();
@@ -26,6 +22,21 @@ int main(int argc, char* argv[])
 	}
 
 	cout << endl;
+}
+
+
+int main(int argc, char* argv[])
+{
+	const char szFormula1[] = "=1+3+5";
+	FormulaParser parser1(szFormula1);
+	parser1.parserToToken1();
+	printParser(parser1);
+
+	const char szFormula2[] = "=IF(R13C3>DATE(2002,1,6),0,IF(ISERROR(R[41]C[2]),0,IF(R13C3>=R[41]C[2],0, IF(AND(R[23]C[11]>=55,R[24]C[11]>=20),R53C3,0))))";
+	FormulaParser parser2(szFormula2);
+	parser2.parserToToken1();
+	printParser(parser2);
+
 
 	return 0;
 }
